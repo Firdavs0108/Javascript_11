60eaee69f96816d91415c9c1169bd4bfa4e485c8
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-02 16:16:01 +0900

    iiii

--- task1.js
+++ task1.js
@@ -1,5 +1,5 @@
 
-let users = [
+let cars = [
    { id:1, year:1998, engine: 1, name: 'Tico'},
    { id:2, year:2005, engine: 1.2, name: 'Matiz'},
    { id:3, year:2010, engine: 1.6, name: 'Gentra'},
@@ -7,27 +7,59 @@ let users = [
    { id:5, year:2012, engine: 2, name: 'Malibu'},
    { id:6, year:2000, engine: 1.2, name: 'Damas'},
    { id:7, year:2018, engine: 2.4, name: 'Tracker'}
-
+   
 ];
 
+
+// // ================================================4
+// Read
+
+// const sortByYear = () => { 
+
+// let key = cars.sort((a,b) => a.year-b.year )
+    
+//     console.log(cars);  }
+//     sortByYear()
+
+// const sortByAlphapet = () => {
+//    let key = cars.sort((a,b) => a.name.localeCompare(b.name))
+//    cars = key
+// }
+// sortByAlphapet()
+// console.log(cars);
+
+
+
 //==========================================1
-//Creat
+    //Update
 
-// let spreading = [...users,{id:8, year:2017, engine: 2.4, name: 'Tracker'} ]
-// console.log(spreading);
+const onUpdate = (ids) => { 
+
+keys = cars.map (value => (value.id === ids.id) ? {...value, ...ids} : value )
+cars = keys
+}
+onUpdate({ id: 2,  name: 'BMW', year:2020})
+console.log(cars);
 
 
+// ============================================2
+//Create
 
-//============================================2
-//Update
+// const addCar = (car) => {
+// cars=[...cars, {id: cars.length+1, ...car}]
 
-// let checking = users.map(value =>  {
-//     if (value.id == 1){
-//         value.name = 'K5'
-//         console.log(users);
-//     }
+// console.log(cars);
+// }
 
-// })
+// addCar({id:8, year:2018, engine: 2.4, name: 'K5'})
+
+
+//////// Create 2-way//////////////
+
+// const addCar = (car) => {
+// cars = [...cars, {...car} ]
+// console.log(cars)}
+// addCar({id:8, year:2018, engine: 2.4, name: 'K5'})
 
 
 
@@ -35,21 +67,15 @@ let users = [
 // //Delete
 
 // const onDelete = (_id_number) => {
-//     let checking = users.filter(value => value.id !== _id_number);
-//     console.log(checking);
-// }
+   //     let checking = users.filter(value => value.id !== _id_number);
+   //     console.log(checking);
+   // }
+   
+   // onDelete(5)
+   
 
-// onDelete(5)
-
-
-
-
-// // ================================================4
-// Read
-
-
-users.map ( users => {
+   
+   
+   
     
-    }) 
     
-    console.log(users);
--- task2.js
+++ task2.js
@@ -2,9 +2,17 @@
 
 
 
-let array = [100, 0, 50, 110, 25]
+// let array = [100, 0, 50, 110, 25]
 
 
 
-console.log(Math.max(...array));
-console.log(Math.min(...array));
+// console.log(Math.max(...array));
+// console.log(Math.min(...array));
+
+
+let arr = [1,[3, [4,[5]]]];
+
+let flatting = arr.flat(Infinity)
+let summing = flatting.reduce((a,b) => a+b, 0)
+
+console.log(summing);
